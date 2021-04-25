57db27763b88ecc3245ae44754a6252beb418159
Marina <marinaradusheva9@gmail.com>
2021-04-25 17:57:08 +0300

    hackerRank c# basics

---
+++ HackerRank/CSharpBasics/Employees/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Solution
+{
+    public class Solution
+    {
+
+        public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+        {
+            Dictionary<string, int> companies = new Dictionary<string, int>();
+            SortedDictionary<string, List<int>> ages = new SortedDictionary<string, List<int>>();
+            foreach (var empl in employees)
+            {
+                if (ages.ContainsKey(empl.Company))
+                {
+                    ages[empl.Company].Add(empl.Age);
+                }
+                else
+                {
+                    ages.Add(empl.Company, new List<int>() { empl.Age });
+                }
+            }
+            foreach (var comp in ages)
+            {
+                companies.Add(comp.Key, (int)Math.Round(comp.Value.Average(x => x)));
+            }
+            return companies;
+        }
+
+        public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+        {
+            SortedDictionary<string, int> empl = new SortedDictionary<string, int>();
+            foreach (var item in employees)
+            {
+                if (empl.ContainsKey(item.Company))
+                {
+                    empl[item.Company]++;
+                }
+                else
+                {
+                    empl.Add(item.Company, 1);
+                }
+            }
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (var com in empl)
+            {
+                result.Add(com.Key, com.Value);
+            }
+            return result;
+        }
+
+        public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+        {
+            SortedDictionary<string, Employee> empl = new SortedDictionary<string, Employee>();
+            foreach (var item in employees)
+            {
+                if (empl.ContainsKey(item.Company))
+                {
+                    if (item.Age>empl[item.Company].Age)
+                    {
+                        empl[item.Company] = item;
+                    }
+                }
+                else
+                {
+                    empl.Add(item.Company, item);
+                }
+            }
+            Dictionary<string, Employee> result = new Dictionary<string, Employee>();
+            foreach (var sth in empl)
+
+            {
+                result.Add(sth.Key, sth.Value);
+            }
+            return result;
+        }
+
+        public static void Main()
+        {
+            int countOfEmployees = int.Parse(Console.ReadLine());
+
+            var employees = new List<Employee>();
+
+            for (int i = 0; i < countOfEmployees; i++)
+            {
+                string str = Console.ReadLine();
+                string[] strArr = str.Split(' ');
+                employees.Add(new Employee
+                {
+                    FirstName = strArr[0],
+                    LastName = strArr[1],
+                    Company = strArr[2],
+                    Age = int.Parse(strArr[3])
+                });
+            }
+
+            foreach (var emp in AverageAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in CountOfEmployeesForEachCompany(employees))
+            {
+                Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+            }
+
+            foreach (var emp in OldestAgeForEachCompany(employees))
+            {
+                Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+            }
+        }
+    }
+
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+    }
+}
---
+++ HackerRank/CSharpBasics/Notes/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution
+{
+
+    public class NotesStore
+    {
+        private List<Note> notes;
+        public NotesStore() 
+        {
+            notes = new List<Note>();
+        }
+        public List<Note> Notes { get => this.notes; private set { } }
+        public void AddNote(String state, String name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentException("Name cannot be empty");
+            }
+            if (state=="completed" || state == "active" || state == "others")
+            {
+                Note newNote = new Note() { Name = name, State = state };
+                this.notes.Add(newNote);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid state {state}");
+            }
+        }
+        public List<String> GetNotes(String state)
+        {
+            if (state == "completed" || state == "active" || state == "others")
+            {
+                List<string> noteNames = new List<string>();
+                for (int i = 0; i < this.Notes.Count; i++)
+                {
+                    if (Notes[i].State==state)
+                    {
+                        noteNames.Add(Notes[i].Name);
+                    }
+                }
+                return noteNames;
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid state {state}");
+            }
+        }
+    }
+    public class Note
+    {
+        public string State { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Solution
+    {
+        public static void Main()
+        {
+            var notesStoreObj = new NotesStore();
+            var n = int.Parse(Console.ReadLine());
+            for (var i = 0; i < n; i++)
+            {
+                var operationInfo = Console.ReadLine().Split(' ');
+                try
+                {
+                    if (operationInfo[0] == "AddNote")
+                        notesStoreObj.AddNote(operationInfo[1], operationInfo.Length == 2 ? "" : operationInfo[2]);
+                    else if (operationInfo[0] == "GetNotes")
+                    {
+                        var result = notesStoreObj.GetNotes(operationInfo[1]);
+                        if (result.Count == 0)
+                            Console.WriteLine("No Notes");
+                        else
+                            Console.WriteLine(string.Join(",", result));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Parameter");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error: " + e.Message);
+                }
+            }
+        }
+    }
+}
